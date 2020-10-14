9c949b9e305dd1d24ef9dd664be51433c35069c8
Dennis_Slobodzian <dennis.slobodzian@baxter-academy.org>
2020-10-14 16:53:15 -0400

    name change

--- src/main/java/org/frc5687/diffswerve/robot/subsystems/DiffSwerveModule.java
+++ src/main/java/org/frc5687/diffswerve/robot/subsystems/DiffSwerveModule.java
@@ -25,7 +25,7 @@ public class DiffSwerveModule {
     private AnalogEncoder _lampreyEncoder;
     private ModuleID _modID;
     private Translation2d _positionVector;
-    private LinearSystem<N3, N2, N2> _swerveModule;
+    private LinearSystem<N3, N2, N2> _swerveModuleModel;
     private KalmanFilter<N3, N2, N2> _swerveObserver;
     private LinearQuadraticRegulator<N3,N2,N2> _swerveController;
     private LinearSystemLoop<N3,N2,N2> _swerveControlLoop;
@@ -39,20 +39,20 @@ public class DiffSwerveModule {
                 _positionVector = new Translation2d(Constants.DriveTrain.WIDTH/2.0, Constants.DriveTrain.LENGTH/2.0);
                 _rightFalcon = new TalonFX(RobotMap.CAN.TALONFX.FR_RIGHT_FALCON);
                 _leftFalcon = new TalonFX(RobotMap.CAN.TALONFX.FR_LEFT_FALCON);
-                _swerveModule = createDifferentialSwerveModule(DCMotor.getFalcon500(2),Constants.DifferentialSwerveModule.FrontRight.INERTIA_STEER,Constants.DifferentialSwerveModule.FrontRight.INERTIA_WHEEL,
+                _swerveModuleModel= createDifferentialSwerveModule(DCMotor.getFalcon500(2),Constants.DifferentialSwerveModule.FrontRight.INERTIA_STEER,Constants.DifferentialSwerveModule.FrontRight.INERTIA_WHEEL,
                         Constants.DifferentialSwerveModule.GEAR_RATIO_STEER, Constants.DifferentialSwerveModule.GEAR_RATIO_WHEEL
                 );
-                _swerveObserver = new KalmanFilter<>(Nat.N3(), Nat.N2(), _swerveModule,
+                _swerveObserver = new KalmanFilter<>(Nat.N3(), Nat.N2(), _swerveModuleModel,
                         Matrix.mat(Nat.N3(),Nat.N1()).fill(Units.degreesToRadians(2),Units.rotationsPerMinuteToRadiansPerSecond(50),Units.rotationsPerMinuteToRadiansPerSecond(50)),
                         Matrix.mat(Nat.N2(),Nat.N1()).fill(Units.degreesToRadians(0.001),Units.rotationsPerMinuteToRadiansPerSecond(3)),
                         0.020
                 );
-                _swerveController = new LinearQuadraticRegulator<>(_swerveModule, VecBuilder.fill(Constants.DifferentialSwerveModule.FrontLeft.Q_AZIMUTH,Constants.DifferentialSwerveModule.FrontLeft.Q_AZIMUTH_ANG_VELOCITY,Constants.DifferentialSwerveModule.FrontLeft.Q_WHEEL_ANG_VELOCITY),
+                _swerveController = new LinearQuadraticRegulator<>(_swerveModuleModel, VecBuilder.fill(Constants.DifferentialSwerveModule.FrontLeft.Q_AZIMUTH,Constants.DifferentialSwerveModule.FrontLeft.Q_AZIMUTH_ANG_VELOCITY,Constants.DifferentialSwerveModule.FrontLeft.Q_WHEEL_ANG_VELOCITY),
                         VecBuilder.fill(12.0, 12.0),
                         0.020
                 );
                 _swerveControlLoop = new LinearSystemLoop<>(
-                        _swerveModule,
+                        _swerveModuleModel,
                         _swerveController,
                         _swerveObserver,
                         12.0,
@@ -63,20 +63,20 @@ public class DiffSwerveModule {
                 _positionVector = new Translation2d(-Constants.DriveTrain.WIDTH/2.0, Constants.DriveTrain.LENGTH/2.0);
                 _rightFalcon = new TalonFX(RobotMap.CAN.TALONFX.FL_RIGHT_FALCON);
                 _leftFalcon = new TalonFX(RobotMap.CAN.TALONFX.FL_LEFT_FALCON);
-                _swerveModule = createDifferentialSwerveModule(DCMotor.getFalcon500(2),Constants.DifferentialSwerveModule.FrontLeft.INERTIA_STEER,Constants.DifferentialSwerveModule.FrontLeft.INERTIA_WHEEL,
+                _swerveModuleModel = createDifferentialSwerveModule(DCMotor.getFalcon500(2),Constants.DifferentialSwerveModule.FrontLeft.INERTIA_STEER,Constants.DifferentialSwerveModule.FrontLeft.INERTIA_WHEEL,
                         Constants.DifferentialSwerveModule.GEAR_RATIO_STEER, Constants.DifferentialSwerveModule.GEAR_RATIO_WHEEL
                 );
-                _swerveObserver = new KalmanFilter<>(Nat.N3(), Nat.N2(), _swerveModule,
+                _swerveObserver = new KalmanFilter<>(Nat.N3(), Nat.N2(), _swerveModuleModel,
                         Matrix.mat(Nat.N3(),Nat.N1()).fill(Units.degreesToRadians(2),Units.rotationsPerMinuteToRadiansPerSecond(50),Units.rotationsPerMinuteToRadiansPerSecond(50)),
                         Matrix.mat(Nat.N2(),Nat.N1()).fill(Units.degreesToRadians(0.001),Units.rotationsPerMinuteToRadiansPerSecond(3)),
                         0.020
                 );
-                _swerveController = new LinearQuadraticRegulator<>(_swerveModule, VecBuilder.fill(Constants.DifferentialSwerveModule.FrontLeft.Q_AZIMUTH,Constants.DifferentialSwerveModule.FrontLeft.Q_AZIMUTH_ANG_VELOCITY,Constants.DifferentialSwerveModule.FrontLeft.Q_WHEEL_ANG_VELOCITY),
+                _swerveController = new LinearQuadraticRegulator<>(_swerveModuleModel, VecBuilder.fill(Constants.DifferentialSwerveModule.FrontLeft.Q_AZIMUTH,Constants.DifferentialSwerveModule.FrontLeft.Q_AZIMUTH_ANG_VELOCITY,Constants.DifferentialSwerveModule.FrontLeft.Q_WHEEL_ANG_VELOCITY),
                         VecBuilder.fill(12.0, 12.0),
                         0.020
                 );
                 _swerveControlLoop = new LinearSystemLoop<>(
-                        _swerveModule,
+                        _swerveModuleModel,
                         _swerveController,
                         _swerveObserver,
                         12.0,
@@ -88,19 +88,19 @@ public class DiffSwerveModule {
                 _positionVector = new Translation2d(Constants.DriveTrain.WIDTH/2.0, -Constants.DriveTrain.LENGTH/2.0);
                 _rightFalcon = new TalonFX(RobotMap.CAN.TALONFX.BR_RIGHT_FALCON);
                 _leftFalcon = new TalonFX(RobotMap.CAN.TALONFX.BR_LEFT_FALCON);
-                _swerveModule = createDifferentialSwerveModule(DCMotor.getFalcon500(2),Constants.DifferentialSwerveModule.BottomRight.INERTIA_STEER,Constants.DifferentialSwerveModule.BottomRight.INERTIA_WHEEL,
+                _swerveModuleModel = createDifferentialSwerveModule(DCMotor.getFalcon500(2),Constants.DifferentialSwerveModule.BottomRight.INERTIA_STEER,Constants.DifferentialSwerveModule.BottomRight.INERTIA_WHEEL,
                     Constants.DifferentialSwerveModule.GEAR_RATIO_STEER, Constants.DifferentialSwerveModule.GEAR_RATIO_WHEEL);
-                _swerveObserver = new KalmanFilter<>(Nat.N3(), Nat.N2(), _swerveModule,
+                _swerveObserver = new KalmanFilter<>(Nat.N3(), Nat.N2(), _swerveModuleModel,
                         Matrix.mat(Nat.N3(),Nat.N1()).fill(Units.degreesToRadians(2),Units.rotationsPerMinuteToRadiansPerSecond(50),Units.rotationsPerMinuteToRadiansPerSecond(50)),
                         Matrix.mat(Nat.N2(),Nat.N1()).fill(Units.degreesToRadians(0.001),Units.rotationsPerMinuteToRadiansPerSecond(3)),
                         0.020
                 );
-                _swerveController = new LinearQuadraticRegulator<>(_swerveModule, VecBuilder.fill(Constants.DifferentialSwerveModule.BottomRight.Q_AZIMUTH,Constants.DifferentialSwerveModule.BottomRight.Q_AZIMUTH_ANG_VELOCITY,Constants.DifferentialSwerveModule.BottomRight.Q_WHEEL_ANG_VELOCITY),
+                _swerveController = new LinearQuadraticRegulator<>(_swerveModuleModel, VecBuilder.fill(Constants.DifferentialSwerveModule.BottomRight.Q_AZIMUTH,Constants.DifferentialSwerveModule.BottomRight.Q_AZIMUTH_ANG_VELOCITY,Constants.DifferentialSwerveModule.BottomRight.Q_WHEEL_ANG_VELOCITY),
                         VecBuilder.fill(12.0, 12.0),
                         0.020
                 );
                 _swerveControlLoop = new LinearSystemLoop<>(
-                        _swerveModule,
+                        _swerveModuleModel,
                         _swerveController,
                         _swerveObserver,
                         12.0,
@@ -111,19 +111,19 @@ public class DiffSwerveModule {
                 _positionVector = new Translation2d(Constants.DriveTrain.WIDTH/2.0, -Constants.DriveTrain.LENGTH/2.0);
                 _rightFalcon = new TalonFX(RobotMap.CAN.TALONFX.BL_RIGHT_FALCON);
                 _leftFalcon = new TalonFX(RobotMap.CAN.TALONFX.BL_LEFT_FALCON);
-                _swerveModule = createDifferentialSwerveModule(DCMotor.getFalcon500(2),Constants.DifferentialSwerveModule.BottomLeft.INERTIA_STEER,Constants.DifferentialSwerveModule.BottomLeft.INERTIA_WHEEL,
+                _swerveModuleModel = createDifferentialSwerveModule(DCMotor.getFalcon500(2),Constants.DifferentialSwerveModule.BottomLeft.INERTIA_STEER,Constants.DifferentialSwerveModule.BottomLeft.INERTIA_WHEEL,
                     Constants.DifferentialSwerveModule.GEAR_RATIO_STEER, Constants.DifferentialSwerveModule.GEAR_RATIO_WHEEL);
-                _swerveObserver = new KalmanFilter<>(Nat.N3(), Nat.N2(), _swerveModule,
+                _swerveObserver = new KalmanFilter<>(Nat.N3(), Nat.N2(), _swerveModuleModel,
                         Matrix.mat(Nat.N3(),Nat.N1()).fill(Units.degreesToRadians(2),Units.rotationsPerMinuteToRadiansPerSecond(50),Units.rotationsPerMinuteToRadiansPerSecond(50)),
                         Matrix.mat(Nat.N2(),Nat.N1()).fill(Units.degreesToRadians(0.001),Units.rotationsPerMinuteToRadiansPerSecond(3)),
                         0.020
                 );
-                _swerveController = new LinearQuadraticRegulator<>(_swerveModule, VecBuilder.fill(Constants.DifferentialSwerveModule.BottomLeft.Q_AZIMUTH,Constants.DifferentialSwerveModule.BottomLeft.Q_AZIMUTH_ANG_VELOCITY,Constants.DifferentialSwerveModule.BottomLeft.Q_WHEEL_ANG_VELOCITY),
+                _swerveController = new LinearQuadraticRegulator<>(_swerveModuleModel, VecBuilder.fill(Constants.DifferentialSwerveModule.BottomLeft.Q_AZIMUTH,Constants.DifferentialSwerveModule.BottomLeft.Q_AZIMUTH_ANG_VELOCITY,Constants.DifferentialSwerveModule.BottomLeft.Q_WHEEL_ANG_VELOCITY),
                         VecBuilder.fill(12.0, 12.0),
                         0.020
                 );
                 _swerveControlLoop = new LinearSystemLoop<>(
-                        _swerveModule,
+                        _swerveModuleModel,
                         _swerveController,
                         _swerveObserver,
                         12.0,
